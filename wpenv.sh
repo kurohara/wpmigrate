40f547947a9475ffa7e9e9032885d265d23eea27
kurohara <kurohara@yk.rim.or.jp>
2015-07-09 10:59:32 +0900

    Japanese WordPress environment does misterious thing(adding MSDOS linebreak at its eol)

--- wpenv.sh
+++ wpenv.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Database name that your WordPress instance uses.
-dbname=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_NAME'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_NAME//"`
+dbname=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_NAME'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_NAME//" -e "s///"`
 
 # User name for the database.
-dbuser=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_USER'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_USER//"`
+dbuser=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_USER'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_USER//" -e "s///"`
 
 # Password for that user.
-dbpasswd=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_PASSWORD'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_PASSWORD//"`
-
+dbpasswd=`egrep -i -e "^[ \t]*define[ \t]*\([ \t]*'DB_PASSWORD'" wp-config.php | sed -e "s/[  ();,']*//g" -e "s/defineDB_PASSWORD//" -e "s///"`
